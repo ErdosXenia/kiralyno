c9bc8b6a7434c0d7f846ea9c3040ee8d2dd4e7a9
ErdosXenia <erdos.xenia@gmail.com>
2020-09-16 08:46:28 +0200

    Add project files.

---
+++ kiralyno/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiralyno
+{
+    class tabla
+    {
+        char[,] T =new char[8,8]; 
+        char Urescella;
+        int UresOszlopokSzama;
+        int UresSorokSzama;
+
+        public tabla(char ch)
+        {
+
+        }
+
+        public void Elhelyez()
+        {
+
+        }
+
+        public void FajlbaIr()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+        }
+
+
+
+        public int UresOszlop()
+        {
+
+        }
+
+        public int UresSor()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
